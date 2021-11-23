1449284d2866f76f252fe1efd413268aafc0dac3
andreysdrv <jeck666@mail.ru>
2021-11-23 10:38:14 +0300

    feat: переписал PostsScreen на классовый компонент

--- src/screens/PostsScreen.js
+++ src/screens/PostsScreen.js
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
+import React, {Component} from 'react';
 import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {useDispatch, useSelector} from 'react-redux';
+import {connect} from 'react-redux';
 import {fetchPosts, removePost} from '../asyncActions/posts';
 import Loader from '../components/Loader';
 
@@ -11,88 +11,95 @@ const randomDate = (start, end, startHour, endHour) => {
   return date;
 };
 
-const PostsScreen = ({navigation}) => {
-  const dispatch = useDispatch();
-  const posts = useSelector(state => state.posts.posts);
-  const isLoading = useSelector(state => state.posts.isLoading);
-
-  useEffect(() => {
-    console.log(posts.length);
-  });
-
-  useEffect(() => {
-    dispatch(fetchPosts());
-    console.log(isLoading);
-  }, []);
-
-  const handleDeletePost = post => {
-    dispatch({type: 'TOGGLE_LOADING', payload: true});
-    dispatch(removePost(post));
-  };
-
-  if (isLoading) {
-    return <Loader />;
+class PostsScreen extends Component {
+  componentDidUpdate() {
+    console.log(this.props.posts.length);
+  }
+  componentDidMount() {
+    this.props.toggleLoading();
+    this.props.getPosts();
   }
 
-  return (
-    <View style={styles.wrapper}>
-      <FlatList
-        onEndReached={() => dispatch(fetchPosts())}
-        onEndReachedThreshold={0.1}
-        initialNumToRender={5}
-        keyExtractor={item => Math.random()}
-        contentContainerStyle={styles.container}
-        data={posts}
-        renderItem={({item}) => (
-          <TouchableOpacity
-            activeOpacity={0.5}
-            style={styles.button}
-            onPress={() => navigation.navigate('CommentsScreen', item)}>
-            {/* <Text style={styles.buttonText}>Comments</Text> */}
-            <View style={styles.post}>
-              <View style={styles.postHeading}>
-                <Text style={styles.text}>
-                  {randomDate(
-                    1012002003045,
-                    1912002003045,
-                    0,
-                    23,
-                  ).toLocaleString()}
-                </Text>
-                <TouchableOpacity
-                  activeOpacity={0.2}
-                  style={styles.button}
-                  onPress={() => handleDeletePost(item)}>
-                  <Text style={{...styles.buttonText, color: '#ed3e58'}}>
-                    Delete
+  render() {
+    if (this.props.isLoading) {
+      return <Loader />;
+    }
+    return (
+      <View style={styles.wrapper}>
+        <FlatList
+          onEndReached={() => this.props.getPosts()}
+          onEndReachedThreshold={0.1}
+          initialNumToRender={5}
+          keyExtractor={item => Math.random()}
+          contentContainerStyle={styles.container}
+          data={this.props.posts}
+          renderItem={({item}) => (
+            <TouchableOpacity
+              activeOpacity={0.5}
+              style={styles.button}
+              onPress={() =>
+                this.props.navigation.navigate('CommentsScreen', item)
+              }>
+              <View style={styles.post}>
+                <View style={styles.postHeading}>
+                  <Text style={styles.text}>
+                    {randomDate(
+                      1012002003045,
+                      1912002003045,
+                      0,
+                      23,
+                    ).toLocaleString()}
                   </Text>
-                </TouchableOpacity>
+                  <TouchableOpacity
+                    activeOpacity={0.2}
+                    style={styles.button}
+                    onPress={() => {
+                      this.props.toggleLoading();
+                      this.props.handleDeletePost(item);
+                    }}>
+                    <Text style={{...styles.buttonText, color: '#ed3e58'}}>
+                      Delete
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <Text style={styles.title}>{item.title}</Text>
               </View>
-              <Text style={styles.title}>{item.title}</Text>
-            </View>
-          </TouchableOpacity>
-        )}
-      />
-    </View>
-  );
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    posts: state.posts.posts,
+    isLoading: state.posts.isLoading,
+  };
 };
 
-export default PostsScreen;
+const mapDispatchToProps = () => {
+  return {
+    getPosts: () => fetchPosts(),
+    toggleLoading: () => ({type: 'TOGGLE_LOADING', payload: true}),
+    handleDeletePost: post => removePost(post),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps())(PostsScreen);
 
 const styles = StyleSheet.create({
   wrapper: {
-    // paddingVertical: 10,
     backgroundColor: '#d1beb0',
     flex: 1,
     alignItems: 'center',
   },
   container: {
-    // width: '95%',
     paddingHorizontal: 10,
   },
   post: {
     width: '100%',
-    // alignSelf: 'center',
     backgroundColor: '#383f51',
     marginVertical: 5,
     borderRadius: 15,
@@ -116,7 +123,3 @@ const styles = StyleSheet.create({
     color: '#d1beb0',
   },
 });
-
-// dddbf1
-// d1beb0
-// 383f51
