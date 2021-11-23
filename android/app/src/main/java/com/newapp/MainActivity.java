4fb9abff6e0a3dc6ced7c6043ec4f4f5da173f4a
andreysdrv <jeck666@mail.ru>
2021-11-23 13:26:49 +0300

    fix: изменил название приложения везде по коду

--- android/app/src/main/java/com/newapp/MainActivity.java
+++ android/app/src/main/java/com/newapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.newapp;
+package com.etm;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "newApp";
+    return "etm";
   }
 
   @Override
--- android/app/src/main/java/com/newapp/MainApplication.java
+++ android/app/src/main/java/com/newapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.newapp;
+package com.etm;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.newapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.etm.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
